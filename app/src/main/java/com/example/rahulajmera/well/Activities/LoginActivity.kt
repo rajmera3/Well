6881b2b10d45270d89a775edb59610eb56a8a44a
Rahul Ajmera <rahulajmera016@gmail.com>
2018-08-21 00:08:00 -0400

    Initial project setup / firebase login

---
+++ app/src/main/java/com/example/rahulajmera/well/Activities/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.example.rahulajmera.well
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.example.rahulajmera.well.ViewModels.LoginViewModel
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity: AppCompatActivity() {
+
+    lateinit var loginViewModel: LoginViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        loginViewModel = LoginViewModel()
+        logInObserver()
+        setContentView(R.layout.activity_login)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        login_button.setOnClickListener { view ->
+            loginViewModel.login(username_text_input.text.toString(), password_text_input.text.toString())
+        }
+    }
+
+    fun logInObserver() {
+        loginViewModel.firebaseLoginObservable.subscribe {e ->
+            Log.d("HERE", e.toString())
+            if (e) {
+
+                var intent = Intent(this, MainActivity::class.java)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/rahulajmera/well/ViewModels/LoginViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.rahulajmera.well.ViewModels
+
+import android.content.Intent
+import android.util.Log
+import com.example.rahulajmera.well.MainActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import io.reactivex.ObservableOnSubscribe
+
+class LoginViewModel {
+
+    private lateinit var auth: FirebaseAuth
+    public lateinit var firebaseLoginObservable: Observable<Boolean>
+    private var loggedIn: Boolean = false
+
+    init {
+        auth = FirebaseAuth.getInstance()
+        firebaseLoginObservable = Observable.create{e ->
+            e.onNext(loggedIn)
+        }
+
+    }
+
+    fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            e: Task<AuthResult> ->
+
+            if (e.isSuccessful) {
+                Log.d("SUCCESS", email)
+                loggedIn = true
+            } else {
+                Log.d("LOGIN", "FAILEEEEED")
+            }
+        }
+
+//        auth.createUserWithEmailAndPassword("hitest@test.com", "hihihi").addOnCompleteListener (this, {
+//            e: Task<AuthResult> ->
+//            if (e.isSuccessful) {
+//                var currentUser: FirebaseUser = auth.currentUser!!
+//                database.child("users").child(currentUser.uid).setValue(currentUser)
+////                    var intent: Intent = Intent(this, PlaidLink::class.java)
+////                    startActivity(intent)
+//            }
+//        })
+    }
+}
